65d109c02bf55848bdabeb52192495d6397aa1cd
JonathanWitting <56074905+JonathanWitting@users.noreply.github.com>
2020-03-18 13:42:23 +0100

    Till Och Med Uppgift 4
    Jonathan WJ Wutv 2020

---
+++ Javascript_Intro.js
@@ -0,0 +1,26 @@
+// Jonathan Witting Jakhammer
+// 18/3 - 2020
+
+function skapaDialog_uppg1(textAttVisa) {
+    alert(textAttVisa);
+}
+
+function authorDialog_uppg2() {
+    alert("Jonathan har skapat denna sida år 2020");
+}
+
+function changeColors_uppg3() {
+    document.getElementById("uppg3_h1").innerHTML = "Jonathan";
+    document.getElementById("uppg3_p").style.backgroundColor = "lightblue";
+}
+
+function changeImage_uppg4() {
+    if (document.getElementById("uppg4_img").alt === "Donald-Duck Card-Image") {
+        document.getElementById("uppg4_img").src = "https://live.staticflickr.com/1324/761769472_a2e23b393d_b.jpg";
+        document.getElementById("uppg4_img").alt = "Donald-Duck Angry-Image";
+    }
+    else if (document.getElementById("uppg4_img").alt === "Donald-Duck Angry-Image") {
+        document.getElementById("uppg4_img").src = "https://live.staticflickr.com/3403/3604860866_e0811023a1_z.jpg";
+        document.getElementById("uppg4_img").alt = "Donald-Duck Card-Image";
+    }
+}
